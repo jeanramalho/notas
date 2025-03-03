72237dc07743dc1086dd9f577305ea68c9baac58
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-03-03 13:06:34 -0300

    [add] - Inicia projeto de notas utilizando userDefaults

---
+++ notas/Controller/NotasViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  notas
+//
+//  Created by Jean Ramalho on 03/03/25.
+//
+
+import UIKit
+
+class NotasViewController: UIViewController {
+    
+    let contentView: NotasView = NotasView()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ notas/View/NotasView.swift
@@ -0,0 +1,19 @@
+//
+//  NotasView.swift
+//  notas
+//
+//  Created by Jean Ramalho on 03/03/25.
+//
+import Foundation
+import UIKit
+
+class NotasView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
