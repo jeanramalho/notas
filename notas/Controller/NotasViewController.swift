e3e8c55529371af945f88bce86aa565135b4d42b
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-03-03 13:13:33 -0300

    [add] - configura viewCode

--- notas/Controller/NotasViewController.swift
+++ notas/Controller/NotasViewController.swift
@@ -21,9 +21,29 @@ class NotasViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
     }
-
+    
+    private func setup(){
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy(){
+        view.addSubview(contentView)
+    }
+    
+    private func setConstraints(){
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: view.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
 
 }
 
--- notas/View/NotasView.swift
+++ notas/View/NotasView.swift
@@ -11,9 +11,24 @@ class NotasView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+        backgroundColor = .red
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy(){
+        
+    }
+    
+    private func setConstraints(){
+        
     }
 }
