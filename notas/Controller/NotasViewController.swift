c1961a95982b57af87718934083717fa0093d470
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-03-05 12:29:20 -0300

    [add] - implementa metodo de abaixar teclado quando clicar em outro lugar da tela que nao seja editavel

--- notas/Controller/NotasViewController.swift
+++ notas/Controller/NotasViewController.swift
@@ -44,6 +44,11 @@ class NotasViewController: UIViewController {
         setConstraints()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    
     func loadingNotas(){
         buscaNotas()
         notasArray = notasSalvas
